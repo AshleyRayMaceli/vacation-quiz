e262acc6c0fcfe52fc039849addb0791962844ce
AshleyRayMaceli <ashley.maceli@gmail.com>
2016-03-25 08:52:25 -0700

    Used simple branching with if/else statements to determing quiz answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
   $("form#quizQuestions").submit(function(event){
-  var question1answer = parseInt($("input:radio[name=answer]:checked").val());
+
+  var question1answer = parseInt($("input:radio[name=question1answer]:checked").val());
+  var question2answer = parseInt($("input:radio[name=question2answer]:checked").val());
+
+  var totalScore = question1answer + question2answer
+
+  if (totalScore < 3) {
+    $(".quizResult1").show();
+  }
+  else if (totalScore < 5) {
+    $(".quizResult2").show();
+  }
+  else {
+    $(".quizResult3").show();
+  }
+
   event.preventDefault();
   });
 });
